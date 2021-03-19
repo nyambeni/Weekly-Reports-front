4e0a0e1bdb38c60466ba6cd5cd5949922ec3497f
unknown <fanieleoleoto@gmail.com>
2021-03-19 10:21:29 +0200

    nm

--- src/app/hod-report/hod-report.page.ts
+++ src/app/hod-report/hod-report.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HodService } from '../hod.service';
 import { Report } from '../report';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { File } from '@ionic-native/file/ngx';
+
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { Platform } from '@ionic/angular';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-hod-report',
@@ -10,7 +18,9 @@ import { Report } from '../report';
 export class HodReportPage implements OnInit {
 
   reports: Report[] = [];
-  constructor(private hodService: HodService) { }
+  pdfObj: any;
+  constructor(private hodService: HodService, private file: File, private plt: Platform,
+    private fileOpener: FileOpener,) { }
 
   ngOnInit() {
     this.hodService.getReports().subscribe(data => {
@@ -20,5 +30,13 @@ export class HodReportPage implements OnInit {
       console.log(this.reports);
     }, error => console.log(error));
   }
+  downloadPdf() {
+    if (this.plt.is('cordova')) {
+
+    } else {
+      this.pdfObj.download();
+    }
+  }
+
 
 }
